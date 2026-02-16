b808969fbbd169bd343601158230e918bca28479
Jean Lima <jcoliveiralima19@gmail.com>
2026-02-16 15:41:20 -0300

    Adicionando troca do src da bandeira

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const alternativasItens = document.querySelectorAll(".lista-alternativas__item");
 const btnProxBandeira = document.querySelector("#btn-prox-bandeira");
+const imagemBandeira = document.querySelector("#imagem-bandeira");
 
 alternativasItens.forEach((alternativa) => {
     alternativa.addEventListener("click", verificaResposta);
@@ -11,19 +12,21 @@ function verificaResposta() {
 
 async function geraQuestao() {
     try {
-        const response = await fetch(`https://restcountries.com/v3.1/all?fields=name,cca3`)
+        const response = await fetch(`https://restcountries.com/v3.1/all?fields=name,cca3,flags`)
 
         if(!response.ok) {
             throw new Error("Requisição falhou! " + response.status)
         }
 
         const data = await response.json();
-        console.log(data);
+        console.log(data[0].flags);
+
+        imagemBandeira.src = data[1].flags.png
+        imagemBandeira.alt = data[1].flags.alt
         
     } catch (error) {
         console.error("Algo deu errado!" + error)
     }
-    
 }
 
 geraQuestao();
