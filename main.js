67a3a43e00b8409151a6624fe6e37b289c817ecc
Jean Lima <jcoliveiralima19@gmail.com>
2026-02-17 13:39:11 -0300

    Preenchendo as alternativas com a opção correta e as incorretas

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function verificaResposta() {
 
 async function geraQuestao() {
     try {
-        const response = await fetch(`https://restcountries.com/v3.1/all?fields=name,cca3,flags`)
+        const response = await fetch(`https://restcountries.com/v3.1/all?fields=name,cca3,flags`);
 
         if(!response.ok) {
             throw new Error("Requisição falhou! " + response.status)
@@ -26,10 +26,25 @@ async function geraQuestao() {
 
         const data = await response.json();
         
-        const numeroAleatorioPais = Math.floor(Math.random() * 251);
+        let numeroAleatorioPais = Math.floor(Math.random() * 250);
+        const numeroAleatorioAlternativa = Math.floor(Math.random() * 3);
         
-        imagemBandeira.src = data[numeroAleatorioPais].flags.png
-        imagemBandeira.alt = data[numeroAleatorioPais].flags.alt
+        imagemBandeira.src = data[numeroAleatorioPais].flags.png;
+        imagemBandeira.alt = data[numeroAleatorioPais].flags.alt;
+        
+        alternativasItens[numeroAleatorioAlternativa].innerHTML = data[numeroAleatorioPais].name.common;
+        alternativasItens[numeroAleatorioAlternativa].classList.remove("nao-preenchida");
+
+        const alternativaNaoPreenchidas = document.querySelectorAll(".lista-alternativas__item.nao-preenchida");
+
+        if (numeroAleatorioPais === 0) {
+            numeroAleatorioPais = numeroAleatorioPais + 2;
+        } else if (numeroAleatorioPais === 249) {
+            numeroAleatorioPais = numeroAleatorioPais - 2;
+        }
+
+        alternativaNaoPreenchidas[0].innerHTML = data[numeroAleatorioPais + 1].name.common;
+        alternativaNaoPreenchidas[1].innerHTML = data[numeroAleatorioPais - 1].name.common;
         
     } catch (error) {
         console.error("Algo deu errado!" + error)
